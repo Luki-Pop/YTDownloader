ed077736e8854adfb359d9aa6c15c6d42fe5581d
Luki-Pop <ds9@o2.pl>
2026-05-07 15:13:30 +0200

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -3,7 +3,12 @@ import os
 import sys
 import shutil
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# --- POPRAWNE USTAWIENIE KATALOGU ROBOCZEGO ---
+if getattr(sys, 'frozen', False):
+    BASE_DIR = os.path.dirname(sys.executable)
+else:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 os.chdir(BASE_DIR)
 
 LOCKFILE = os.path.join(BASE_DIR, "app.lock")
