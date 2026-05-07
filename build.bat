45aeef011dc682ee82ba2703b79d97e3eeb7db64
Luki-Pop <ds9@o2.pl>
2026-05-07 15:43:05 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,17 @@
 @echo off
 title Building YTDownloader
 
+echo Reading current version...
+set /p VERSION=<version.txt
+
+for /f "tokens=1,2,3 delims=." %%a in ("%VERSION%") do (
+    set /a PATCH=%%c+1
+    set NEW_VERSION=%%a.%%b.!PATCH!
+)
+
+for /f %%i in ('powershell -command "Get-Date -Format yyyy-MM-dd"') do set DATE=%%i
+echo %NEW_VERSION% (%DATE%)>version.txt
+
 echo Cleaning old build...
 rmdir /s /q build 2>nul
 rmdir /s /q dist 2>nul
@@ -14,5 +25,5 @@ copy /Y dist\launcher.exe YTDownloader.exe >nul
 echo Building installer...
 "C:\Program Files (x86)\NSIS\makensis.exe" installer.nsi
 
-echo DONE!
+echo DONE! Version %NEW_VERSION%
 pause
